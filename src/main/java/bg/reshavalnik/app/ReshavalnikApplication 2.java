15991498ae126ba4f4434e045c1ace6d726ba926
VKoparov <valentinkoparov@gmail.com>
2025-04-18 17:01:04 +0300

    Initial commit

---
+++ src/main/java/bg/reshavalnik/app/ReshavalnikApplication 2.java
@@ -0,0 +1,12 @@
+package bg.reshavalnik.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReshavalnikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReshavalnikApplication.class, args);
+    }
+}
