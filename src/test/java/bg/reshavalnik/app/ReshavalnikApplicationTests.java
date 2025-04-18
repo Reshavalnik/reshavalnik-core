594259c5402508e633f18aaeddfd636b84faa7a3
VKoparov <valentinkoparov@gmail.com>
2025-04-18 17:01:04 +0300

    Initial commit

---
+++ src/test/java/bg/reshavalnik/app/ReshavalnikApplicationTests.java
@@ -0,0 +1,11 @@
+package bg.reshavalnik.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReshavalnikApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
