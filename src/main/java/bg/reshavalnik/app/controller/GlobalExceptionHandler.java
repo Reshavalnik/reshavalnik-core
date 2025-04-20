8f913ce72784eb2cd41ba6f0b8ec11856019c947
nikeza <nikeza@gmail.com>
2025-04-20 19:43:05 +0300

    feature - implement controller and repository

---
+++ src/main/java/bg/reshavalnik/app/controller/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package bg.reshavalnik.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
---
+++ src/main/java/bg/reshavalnik/app/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package bg.reshavalnik.app.repository;
+
+import bg.reshavalnik.app.security.domain.User;
+import bg.reshavalnik.app.security.dto.projection.UserDetailProjection;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    boolean existsByUsername(String username);
+
+    Optional<UserDetailProjection> getUserByUsername(String username);
+}
