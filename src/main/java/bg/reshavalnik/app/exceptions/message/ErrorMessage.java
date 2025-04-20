72709fbf352cf83cab6034f664f9d2e3384ec20f
nikeza <nikeza@gmail.com>
2025-04-20 19:40:38 +0300

    feature - implement anotation, exeptions, mapper, service, validators

---
+++ src/main/java/bg/reshavalnik/app/exceptions/message/ErrorMessage.java
@@ -0,0 +1,8 @@
+package bg.reshavalnik.app.exceptions.message;
+
+public class ErrorMessage {
+
+    public static final String WRONG_EMAIL_OR_PASSWORD = "Wrong email or password!";
+    public static final String USERNAME_IS_ALREADY_TAKEN = "Email is already taken!";
+    public static final String CANNOT_ENTER_AS_ADMIN = "Cannot enter as: ";
+}
