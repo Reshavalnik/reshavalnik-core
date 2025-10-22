2eaae680054a446403e5153516308b047b2f9d9e
VKoparov <valentinkoparov@gmail.com>
2025-10-22 15:25:29 +0300

    wip auth 1

--- src/main/java/bg/reshavalnik/app/repository/user/UserRepository.java
+++ src/main/java/bg/reshavalnik/app/repository/user/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<UserDetailProjection> getUserByUsername(String username);
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
